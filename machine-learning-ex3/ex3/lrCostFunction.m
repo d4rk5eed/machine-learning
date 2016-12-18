aeaf8296cb0ef1ba34e6d1e1dbefaaecb1279307
Yaroslav Zemlyanuhin <d4rk5eed@yandex.ru>
2016-12-19 00:27:16 +0300

    ==                                   Part Name |     Score | Feedback ==                                   --------- |     ----- | -------- ==             Regularized Logistic Regression |  30 /  30 | Nice work! ==              One-vs-All Classifier Training |  20 /  20 | Nice work! ==            One-vs-All Classifier Prediction |   0 /  20 | ==          Neural Network Prediction Function |   0 /  30 | ==                                   -------------------------------- ==                                             |  50 / 100 | ==

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -35,6 +35,7 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
+
 J = (sum( -1 * y' * log(sigmoid(X* theta)) - (1 - y)' * log(1 - sigmoid(X* theta)) )) / m + (lambda / (2*m)) * sum(theta(2:end) .^ 2)
 
 grad = (1/m) * sum( (sigmoid(X * theta)- y) .* X)
