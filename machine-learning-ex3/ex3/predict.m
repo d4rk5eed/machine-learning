152987064f3b31e43cea00da4ee9aada47b933ad
Yaroslav Zemlyanuhin <d4rk5eed@yandex.ru>
2016-12-21 21:32:57 +0300

    == ==                                   Part Name |     Score | Feedback ==                                   --------- |     ----- | -------- ==             Regularized Logistic Regression |  30 /  30 | Nice work! ==              One-vs-All Classifier Training |  20 /  20 | Nice work! ==            One-vs-All Classifier Prediction |  20 /  20 | Nice work! ==          Neural Network Prediction Function |  30 /  30 | Nice work! ==                                   -------------------------------- ==                                             | 100 / 100 |

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -1,4 +1,5 @@
 function p = predict(Theta1, Theta2, X)
+
 %PREDICT Predict the label of an input given a trained neural network
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
@@ -7,12 +8,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,12 +22,13 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = [ones(size(X,1), 1) X]
 
+a2 = sigmoid(X * Theta1')
+a2 = [ones(size(a2,1),1) a2]
 
-
-
-
-
+z = a2 * Theta2'
+[probs, p] = max(sigmoid(z), [], 2)
 
 
 % =========================================================================
